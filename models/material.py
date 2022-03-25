789ecdaf0f48c75aebba9f49d704c48c5703925b
jesse-ev <jesse.evans@hashmicro.com>
2022-03-25 09:44:36 +0700

    Fix for materials

--- models/material.py
+++ models/material.py
@@ -5,13 +5,14 @@ class Material(models.Model):
     _name = 'black.mesa.material'
     _description = 'Material'
 
-    name = fields.Char(string='Name')
-    qty = fields.Integer(string='Quantity')
+    name = fields.Char(string='Name', required=True)
+    qty = fields.Integer(string='Quantity', required=True)
     description = fields.Text(string='Description')
     
     source = fields.Selection(
         string='Source of Material',
-        selection=[('earth', 'Earth'), ('xen', 'Xen')]
+        selection=[('earth', 'Earth'), ('xen', 'Xen')],
+        required=True
     )
 
     image = fields.Binary(string='Image')
