64c01f1e9c9adbf712cabd1df3090e03f021d3ea
10thjeh <glock.evans8@gmail.com>
2022-04-04 17:13:24 +0700

    Update description and icon

--- __manifest__.py
+++ __manifest__.py
@@ -1,23 +1,22 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "black_mesa",
+    'name': "Black mesa manufacturing",
 
     'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+        A pseudo-manufacturing system for Black Mesa""",
 
     'description': """
-        Long description of module's purpose
+        This module is created for training purpose, this module serves as custom-module making assignment for internship.
     """,
 
-    'author': "My Company",
+    'author': "Je-Bot Company",
     'website': "http://www.yourcompany.com",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Uncategorized',
-    'version': '1.0',
+    'version': '1.1',
     'application':True,
 
     # any module necessary for this one to work correctly
